e02417dcc8830db30bd51e6b53a0249a1a545776
Knaito163 <deen.sea210@gmail.com>
2017-11-02 15:20:15 +0900

    First Commit

---
+++ edisonNativeApp/Sensor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace edisonNativeApp
+{
+    public class Sensor
+    {
+        public Sensor()
+        {
+        }
+        public float Temp { get; set; }
+        public float Humidity { get; set; }
+        public bool Presence { get; set; }
+    }
+}
---
+++ edisonNativeApp/edisonNativeAppPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Xamarin.Forms;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+using Newtonsoft.Json;
+
+namespace edisonNativeApp
+{
+    public partial class edisonNativeAppPage : ContentPage
+    {
+        public edisonNativeAppPage()
+        {
+            InitializeComponent();
+        }
+
+        // ボタンメソッド
+        public async void getSensors(object sender, System.EventArgs e)
+        {
+            HttpClient client;
+            client = new HttpClient();
+            var api_uri = "xxxxxxx";
+            var uri = new Uri(api_uri);
+
+            // HTTPリクエスト
+            var response = await client.GetAsync(uri);
+            //System.Diagnostics.Debug.WriteLine(response);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                //System.Diagnostics.Debug.WriteLine(content);
+                var sensors = JsonConvert.DeserializeObject<Sensor>(content);
+                System.Diagnostics.Debug.WriteLine(sensors.Temp);
+
+                this.te.Detail = sensors.Temp.ToString();
+                this.hu.Detail = sensors.Humidity.ToString();
+                this.pr.Detail = sensors.Presence ? "いる" : "いない";
+
+            }
+        }
+    }
+}
